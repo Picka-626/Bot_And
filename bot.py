9a87acaa5bde6b6260cebe715334b4f4ebaea5fa
Picka-626 <128507398+Picka-626@users.noreply.github.com>
2025-09-25 13:13:21 +0200

    Update bot.py
    removed keep alive

--- bot.py
+++ bot.py
@@ -12,24 +12,6 @@ import os
 import pytz
 import os
 
-from flask import Flask
-from threading import Thread
-
-# ===================================== KEEP ALIVE =====================================
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot is alive!"
-
-def run():
-    app.run(host="0.0.0.0", port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
-
 # ===================================== BOT SETUP =====================================
 intents = discord.Intents.default()
 intents.message_content = True
@@ -320,6 +302,5 @@ async def on_ready():
 
 
 # ====================== RUN ======================
-keep_alive()
 bot.run(os.getenv("DISCORD_TOKEN"))
 
